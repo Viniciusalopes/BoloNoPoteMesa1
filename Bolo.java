d52eabb952ee09cb3124d2d89addd5c944e98874
Gustavo-404 <57229483+Gustavo-404@users.noreply.github.com>
2019-11-07 13:30:40 -0300

    Add files via upload
    SEM O SCANNER !

--- Bolo.java
+++ Bolo.java
@@ -1,16 +1,11 @@
 
 package teste;
 
-import java.util.Scanner;
-
-/**
- *
- * @author Gustavo Gabriel
- */
+// @author Gustavo Gabriel
+ 
 public class Bolo {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        
+   
         System.out.println("\"LISTA DE INGREDIENTES de acordo com a receita\"");
     }
     
