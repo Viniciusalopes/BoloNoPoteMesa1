25a4ac2e415290f292dbf7c2d0d747f27b12fa5f
Gustavo-404 <57229483+Gustavo-404@users.noreply.github.com>
2019-11-07 13:55:45 -0300

    Add files via upload
    validação verificando se o double é maior que  0 !

--- Bolo.java
+++ Bolo.java
@@ -2,11 +2,23 @@
 package teste;
 
 // @author Gustavo Gabriel
+
+import java.util.Scanner;
+
  
 public class Bolo {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+      
+        
+        double x =input.nextDouble();
+        while(x<=0){
+            System.out.println("Erro! O valor informado deve ser maior que 0\nDigite novamente : ");
+            x=input.nextDouble();
+        }
+    }
    
-        System.out.println("\"LISTA DE INGREDIENTES de acordo com a receita\"");
+       
     }
     
-}
+
