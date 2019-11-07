ca5914204c1f25a0e75d65e15a2a5a860c30de10
Gustavo-404 <57229483+Gustavo-404@users.noreply.github.com>
2019-11-07 13:17:58 -0300

    Add files via upload

---
+++ Bolo.java
@@ -0,0 +1,17 @@
+
+package teste;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gustavo Gabriel
+ */
+public class Bolo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("\"LISTA DE INGREDIENTES de acordo com a receita\"");
+    }
+    
+}
