f545b3a6aba6e33fd76a00627b86a51788c96746
renanrpads <57493940+renanrpads@users.noreply.github.com>
2019-11-07 22:19:07 -0300

    Add files via upload

--- Fase2Etapa1.java
+++ Fase2Etapa1.java
@@ -1,16 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Exercicios;
 
-import java.util.Scanner;
+
 
 public class Fase2Etapa1 {
 
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
+        
 
         System.out.println("Viabilidade das receitas:");
         System.out.println("1 - Nome_da_receita_1");
