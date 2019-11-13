ca7e6a131923ad5ec43dcf6e144b7495e5ef3c99
Viniciusalopes Tecnologia <33704906+Viniciusalopes@users.noreply.github.com>
2019-11-13 00:39:42 -0300

    Fase4_Etapa3 - gugavasco

--- src/app/Principal.java
+++ src/app/Principal.java
@@ -85,9 +85,6 @@ import java.util.Scanner;
 
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
 
         // Fase1_Etapa1 - miguelneto -----------------------------------------------------
@@ -95,9 +92,42 @@ public class Principal {
 
         int NumReceitas = 3;
         double[][] receita1 = new double[9][2];//Matrizes correspondente a cada receita @GUSTAVO G.
-        double[][] receita2 = new double[9][2];//Miguel ja avia criado corretamente!
-        double[][] receita3 = new double[7][2];
+        double[][] receita2 = new double[8][2];//Miguel ja avia criado corretamente!
+        double[][] receita3 = new double[9][2];
+      
+        double[] ingrediente_receita1 = new double[9];
+        double[] ingrediente_receita2 = new double[8];
+        double[] ingrediente_receita3 = new double[9];
+        
+        ingrediente_receita1[0] = 300.0;
+        ingrediente_receita1[1] = 300.0;
+        ingrediente_receita1[2] = 4.0;
+        ingrediente_receita1[3] = 300.0;
+        ingrediente_receita1[4] = 600.0;
+        ingrediente_receita1[5] = 600.0;
+        ingrediente_receita1[6] = 600.0;
+        ingrediente_receita1[7] = 20.0;
+        ingrediente_receita1[8] = 15.0;
 
+        ingrediente_receita2[0] = 300.0;
+        ingrediente_receita2[1] = 300.0;
+        ingrediente_receita2[2] = 4.0;
+        ingrediente_receita2[3] = 300.0;
+        ingrediente_receita2[4] = 600.0;
+        ingrediente_receita2[5] = 600.0;
+        ingrediente_receita2[6] = 600.0;
+        ingrediente_receita2[7] = 15.0;
+
+        ingrediente_receita3[0] = 500.0;
+        ingrediente_receita3[1] = 300.0;
+        ingrediente_receita3[2] = 10.0;
+        ingrediente_receita3[3] = 300.0;
+        ingrediente_receita3[4] = 480.0;
+        ingrediente_receita3[5] = 20.0;
+        ingrediente_receita3[6] = 250.0;
+        ingrediente_receita3[7] = 50.0;
+        ingrediente_receita3[8] = 15.0;
+      
         int a = 0; // indice manipular vetor receita1; 
         int b = 0; // indice manipular vetor receita2;
         int c = 0; // indice manipular vetor receita3;
@@ -412,12 +442,6 @@ public class Principal {
                 break;
         }
 
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 2; j++) {
-                System.out.print(receita1[i][j] + " ");
-            }
-            System.out.println("");
-        }
         // FIM Fase3_Etapa1 - gustavo  -----------------------------------------------------
 
         /*
@@ -437,13 +461,80 @@ public class Principal {
         //
         //
         /* Fase4_Etapa3 -------------------------------------------------------------------
+         Calcular o custo da receita
+         Calcular o custo de um bolo
+         Verificar se a receita é viável ou não, porque e % de lucro
+        */
+        double qtd;
+        double vlr;
+        double rec;
+        double custo_ingrediente = 0.0;
+        double custo_receita = 0.0;
+        double custo_unitario = 0.0;
 
-            Calcular o custo da receita
-            Calcular o custo de um bolo
-            Verificar se a receita é viável ou não, porque e % de lucro
-        
-        FIM Fase4_Etapa3 -------------------------------------------------------------------
-         */
+        if (opc == 1) {
+            for (int i = 0; i < receita1.length; i++) {
+                qtd = receita1[i][0];
+                vlr = receita1[i][1];
+                rec = ingrediente_receita1[i];
+
+                // conta custo unitario
+                custo_ingrediente = (rec * vlr) / qtd;
+
+                // adiciona o valor da conta na soma
+                custo_receita = custo_receita + custo_ingrediente;
+            }
+        }
+        if (opc == 2) {
+            for (int i = 0; i < receita2.length; i++) {
+                qtd = receita2[i][0];
+                vlr = receita2[i][1];
+                rec = ingrediente_receita2[i];
+
+                // conta custo unitario
+                custo_ingrediente = (rec * vlr) / qtd;
+
+                // adiciona o valor da conta na soma
+                custo_receita = custo_receita + custo_ingrediente;
+            }
+
+        }
+
+        if (opc == 3) {
+            for (int i = 0; i < receita3.length; i++) {
+                qtd = receita3[i][0];
+                vlr = receita3[i][1];
+                rec = ingrediente_receita3[i];
+
+                // conta custo unitario
+                custo_ingrediente = (rec * vlr) / qtd;
+
+                // adiciona o valor da conta na soma
+                custo_receita = custo_receita + custo_ingrediente;
+            }
+
+        }
+
+        custo_unitario = custo_receita / 15;
+        System.out.printf("Qual o valor de venda do pote ? ");
+
+        valor_venda = entrada.nextDouble();
+        lucro_real = valor_venda - custo_unitario;
+        lucro_porcent = (lucro_real / custo_unitario) * 100;
+
+        if (custo_receita <= 30.0) {
+
+            System.out.printf("A receita é viável !!");
+            System.out.printf("O custo de cada bolo é : R$ %.2f", custo_unitario);
+            System.out.printf("Lucro em dinheiro por unidade é : R$ %.2f\n", lucro_real);
+            System.out.printf("Lucro em porcentagem é : %.2f %%\n", lucro_porcent);
+        } else {
+
+            System.out.println("A receita não é viável !!");
+        }
+
+
+        /*FIM Fase4_Etapa3 -------------------------------------------------------------------
         //
         //
         //
